9b880e884e6a5fa37ee385d7a69e21cb963d7615
sumukh <50351727+skywalker5014@users.noreply.github.com>
2024-01-13 00:42:34 +0530

    Update razorpayInit.js

--- backend/payment_gateway/razorpayInit.js
+++ backend/payment_gateway/razorpayInit.js
@@ -1,8 +1,8 @@
 import Razorpay from 'razorpay';
 
 const instance = new Razorpay({
-    key_id: 'rzp_test_WqpdjU6uMUVCvC',
-    key_secret: 'oq70kmZLu7CLAR5QhxnyRrTw'
+    key_id: 'your_id',
+    key_secret: 'your_secret'
 });
 
 export default instance;
